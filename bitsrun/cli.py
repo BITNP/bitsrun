978dcc8a109be7448c4287922f13eaa5667c1883
spencerwooo <spencer.woo@outlook.com>
2023-04-04 11:13:29 +0800

    refactor:  with  creates JSON output regardless of login state

--- bitsrun/cli.py
+++ bitsrun/cli.py
@@ -47,16 +47,18 @@ def status(json: bool):
     """Check current network login status."""
     login_status = get_login_status()
 
-    if login_status.get("user_name"):
-        if json:
-            print(dumps(login_status))
-        else:
-            click.echo(
-                click.style("bitsrun: ", fg="green")
-                + f"{login_status['user_name']} ({login_status['online_ip']}) is online"
-            )
-            print_status_table(login_status)
+    # Output in JSON format if `--json` is passed, then exit
+    if json:
+        print(dumps(login_status))
+        return
 
+    # Output in human-readable format
+    if login_status.get("user_name"):
+        click.echo(
+            click.style("bitsrun: ", fg="green")
+            + f"{login_status['user_name']} ({login_status['online_ip']}) is online"
+        )
+        print_status_table(login_status)
     else:
         click.echo(
             click.style("bitsrun: ", fg="cyan")
