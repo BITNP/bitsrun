829381eb65eba9d02c628641168cce36c43d20ff
Y.D.X. <73375426+YDX-2147483647@users.noreply.github.com>
2024-04-07 12:34:45 +0800

    style: `ruff format`

--- src/bitsrun/user.py
+++ src/bitsrun/user.py
@@ -48,7 +48,9 @@ class User:
         # use http to visit another site, redirect by srun to 10.0.0.55 to get url params (ac_id, theme, wlanuserip, etc.)
         # but better to check since the user may have been authenticated
         resp = self.client.get('/', follow_redirects=True)
-        resp_valid = httpx.Client(base_url='http://www.bit.edu.cn').get('/', follow_redirects=True)
+        resp_valid = httpx.Client(base_url='http://www.bit.edu.cn').get(
+            '/', follow_redirects=True
+        )
         if resp_valid.url.params.get('ac_id') is None:
             self.acid = resp.url.params.get('ac_id')
         else:
